36eb555eba53ca82135a6c83757473942e030f6f
mcarlhenrik <mcarlhenrik@gmail.com>
2012-11-15 15:00:34 +0100

    Updating Animal

--- src/Animal.java
+++ src/Animal.java
@@ -7,5 +7,17 @@ public class Animal {
 	public void eating() {
 		System.out.println("Eating");
 	}
+	
+	public void howHappy() {
+		int howHappy = 100;
+	}
+	
+	public void howTired() {
+		int howTired = 100;
+	}
+	
+	public void howHungry() {
+		int howHungry = 100;
+	}
 
 }
