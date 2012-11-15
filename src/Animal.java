ba83d25ad8d5e18069737568a7237a1ce7aab573
mcarlhenrik <mcarlhenrik@gmail.com>
2012-11-15 15:26:14 +0100

    Updating Animal

--- src/Animal.java
+++ src/Animal.java
@@ -1,39 +1,21 @@
 
 public class Animal {
-/*
-	public void sleep() {
-		System.out.println("Sleeping");
-	}
+
+	int currentHunger = 0;
+	int howSleepy = 0;
+	int howHappy = 100;
 	
-	// A method named eating
-	public void eating() {
-		System.out.println("Eating");
-	}
-	
-	public void howHappy() {
-		int howHappy = 100;
-	}
-	
-	public void howTired() {
-		int howTired = 100;
-	}
-	
-	public void howHungry() {
-		int howHungry = 100;
-	}
-*/
-	int currentHunger;
-	int howSleepy;
-	int speed;
-	
+	// A setter for hungerValue
 	public void setHunger(int hungerValue) {
         currentHunger = hungerValue;
     }
-        
+    
+	// A setter for sleepyValue
     public void setHowSleepy(int sleepyValue) {
         howSleepy = sleepyValue;
     }
-        
+    
+    // Methods to update currentHunger
     public void decreaseHunger(int hungerDecrement) {
         currentHunger -= hungerDecrement;
     }
