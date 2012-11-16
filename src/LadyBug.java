cf5002b9526cbb261a4dc82c402c20f1536a9c51
mcarlhenrik <mcarlhenrik@gmail.com>
2012-11-16 11:35:21 +0100

    jk

--- src/LadyBug.java
+++ src/LadyBug.java
@@ -1,10 +1,17 @@
 
 public class LadyBug extends Animal {
-	public LadyBug() {
-		String name = "Ms Super Lady Buggy";
-		String line01 = "I'm a ladybug! and my name is " + name;
+	
+	// An constructor that also takes a name and present it
+	public LadyBug(String name) {
+		String line01 = "I'm a ladybug! And my name is " + name;
 		
 		speaking(line01);
 	}
+	
+//	@Override
+//	public void 
 
+	public void fly() {
+		
+	}
 }
