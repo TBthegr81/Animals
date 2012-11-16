7c915a9d4d524bf00bd7614087b8f985136e4c7f
mcarlhenrik <mcarlhenrik@gmail.com>
2012-11-16 11:08:50 +0100

    Adding the subclass LadyBug

---
+++ src/LadyBug.java
@@ -0,0 +1,10 @@
+
+public class LadyBug extends Animal {
+	public LadyBug() {
+		String name = "Ms Super Lady Buggy";
+		String line01 = "I'm a ladybug! and my name is " + name;
+		
+		speaking(line01);
+	}
+
+}
